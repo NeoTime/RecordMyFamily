5faf614471c47301db0591d293b1f1835c306039
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-26 20:16:12 -0800

    Created the UI of the App

---
+++ RecordMyFamily/FamilySoundViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FamilySoundViewController.swift
+//  RecordMyFamily
+//
+//  Created by Jefferey Rigler on 11/26/16.
+//  Copyright © 2016 RiglerDigital. All rights reserved.
+//
+
+import UIKit
+
+class FamilySoundViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var recordButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
